2595be3f2f077bd045818bbcb93118de390df15c
Vlad <vlad_tunik@mail.ru>
2023-03-13 14:21:27 +0200

    xx-3. view configured

--- M17_Concurrency/ViewController.swift
+++ M17_Concurrency/ViewController.swift
@@ -12,22 +12,65 @@ class ViewController: UIViewController {
     
     let service = Service()
     
-    private lazy var imageView: UIImageView = {
-        let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        view.contentMode = .scaleAspectFit
+    private lazy var firstImageView: UIImageView = {
+        let view = UIImageView()
+//        view.image = UIImage(named: "1")
+        view.contentMode = .scaleToFill
+        return view
+    }()
+    private lazy var secondeImageView: UIImageView = {
+        let view = UIImageView()
+//        view.image = UIImage(named: "1")
+        view.contentMode = .scaleToFill
+        return view
+    }()
+    private lazy var thirdImageView: UIImageView = {
+        let view = UIImageView()
+//        view.image = UIImage(named: "1")
+        view.contentMode = .scaleToFill
         return view
     }()
     
-    private lazy var activityIndicator: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView(frame: CGRect(x: 220, y: 220, width: 140, height: 140))
+    private lazy var fourthImageView: UIImageView = {
+        let view = UIImageView()
+//        view.image = UIImage(named: "1")
+        view.contentMode = .scaleToFill
         return view
+    }()
+    
+    private lazy var fifthImageView: UIImageView = {
+        let view = UIImageView()
+//        view.image = UIImage(named: "1")
+        view.contentMode = .scaleToFill
+        return view
+    }()
+    
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.addArrangedSubview(firstImageView)
+        stackView.addArrangedSubview(secondeImageView)
+        stackView.addArrangedSubview(thirdImageView)
+        stackView.addArrangedSubview(fourthImageView)
+        stackView.addArrangedSubview(fifthImageView)
+        
+        return stackView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(imageView)
+        view.addSubview(stackView)
         view.addSubview(activityIndicator)
+        setupConstraints()
         activityIndicator.startAnimating()
         onLoad()
     }
@@ -40,10 +83,47 @@ class ViewController: UIViewController {
                 return
             }
             
-            let image = self.service.loadImage(urlString: urlString)
-            self.imageView.image = image
+//            let imageOne = self.service.loadImage(urlString: urlString)
+//            self.firstImageView.image = imageOne
+//            let imageTwo = self.service.loadImage(urlString: urlString)
+//            self.secondeImageView.image = imageTwo
+//            let imageThree = self.service.loadImage(urlString: urlString)
+//            self.thirdImageView.image = imageThree
+//            let imageForth = self.service.loadImage(urlString: urlString)
+//            self.fourthImageView.image = imageForth
+//            let imageFive = self.service.loadImage(urlString: urlString)
+//            self.fifthImageView.image = imageFive
             
             self.activityIndicator.stopAnimating()
+        }
+    }
+    
+    private func setupConstraints() {
+        activityIndicator.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        secondeImageView.snp.makeConstraints { make in
+            make.height.equalTo(firstImageView)
+        }
+        thirdImageView.snp.makeConstraints { make in
+            make.height.equalTo(firstImageView)
+        }
+        fourthImageView.snp.makeConstraints { make in
+            make.height.equalTo(firstImageView)
+        }
+        fifthImageView.snp.makeConstraints { make in
+            make.height.equalTo(firstImageView)
         }
     }
 }
